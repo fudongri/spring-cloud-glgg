30c2fba7bfd4ed1e223c112a7a087afa05d6e941
fudongri <393556397@qq.com>
2017-08-20 18:59:53 +0800

    服务共用配置分离

--- config-server/src/main/java/org/galigeigei/ConfigServerApplication.java
+++ config-server/src/main/java/org/galigeigei/ConfigServerApplication.java
@@ -2,10 +2,13 @@ package org.galigeigei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
+
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
